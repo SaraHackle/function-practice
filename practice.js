0d0197116aa23cb665dd8d34eb6947d413a451d0
Sara Hackle <Shackle@ivytech.edu>
2021-11-23 14:40:23 -0600

    created an addition function that adds three numbers passed as arguments

--- practice.js
+++ practice.js
@@ -1,14 +1,12 @@
-
 //practice example of a function that calculates age of a person in dog years
 const calculateAgeInDogYears = (ageOfPerson) => {
-    const ageInDogYears = ageOfPerson / 7
-    
-    return ageInDogYears
+  const ageInDogYears = ageOfPerson / 7;
 
-}
+  return ageInDogYears;
+};
 
-const dogAge = calculateAgeInDogYears(45)
-console.log(dogAge)
+const dogAge = calculateAgeInDogYears(45);
+console.log(dogAge);
 
 // Write a function that takes a string of a dog breed as an argument (like 'border collie')
 // Have the function return the value of "My favorite dog breed is schnauzer" if an argument of "schnauzer" is provided.
@@ -17,15 +15,27 @@ console.log(dogAge)
 // If, and only if, an argument value of "meow" is provided to the function, it should return the string "I like cats".
 
 const makeFavoriteString = (dogBreed) => {
-    if (dogBreed === "meow") {
-    const faveDogString = 'I like cats'
-    return faveDogString
-    } else {
-    let faveDogString = `my favorite dog breed is ${dogBreed}`
-    return faveDogString
-    }
+  if (dogBreed === "meow") {
+    const faveDogString = "I like cats";
+    return faveDogString;
+  } else {
+    let faveDogString = `my favorite dog breed is ${dogBreed}`;
+    return faveDogString;
+  }
+};
+const currentFaveString = makeFavoriteString("meow");
+console.log("When it comes to pets,", currentFaveString);
 
-}
-const currentFaveString = makeFavoriteString("meow")
-console.log( "When it comes to pets,",  currentFaveString)
+// In this exercise, you are going to define more than one parameter for functions.
 
+// Write a function named add that returns the sum of two numbers.
+// Log the result of the add function.
+// Modify the function to return the sum of three numbers.
+// If the values of 17, 4, and 11 are provided as arguments, then the function must evaluate to 32.
+
+const add = (num1, num2, num3) => {
+  const sum = num1 + num2 + num3;
+  return sum;
+};
+const currentSum = add(17, 4, 11);
+console.log("Current Sum:", currentSum);
